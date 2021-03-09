46f7c41f58cba2ac5f8ba0f7d4df47b0708c4ea2
Otávio Luis Martins <otavioluism97@hotmail.com>
2021-03-09 16:45:22 -0300

    created DASHBOARD with your CSS and HTML

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,68 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles'
+import logoImg from '../../assets/logo_header_github.svg';
+
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
   return (
-    <Title>
-      Explore repositório do GitHub
-    </Title>
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title> Explore repositório do GitHub </Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório"/>
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+          <a href="">
+
+            <img src="https://avatars.githubusercontent.com/u/43592310?s=460&u=48323dabcc77b148e26878bc5e0ae015b4618c4f&v=4"
+            alt="Avatar"
+            />
+
+            <div>
+              <strong>Rockeseat_unform</strong>
+              <p>Este repositori foi criado com o intuiro de levar conhecimento aos usuarios. </p>
+            </div>
+
+            <FiChevronRight size={20} />
+
+          </a>
+
+          <a href="">
+
+            <img src="https://avatars.githubusercontent.com/u/43592310?s=460&u=48323dabcc77b148e26878bc5e0ae015b4618c4f&v=4"
+            alt="Avatar"
+            />
+
+            <div>
+              <strong>Rockeseat_unform</strong>
+              <p>Este repositori foi criado com o intuiro de levar conhecimento aos usuarios. </p>
+            </div>
+
+            <FiChevronRight size={20} />
+
+          </a>
+
+          <a href="">
+
+            <img src="https://avatars.githubusercontent.com/u/43592310?s=460&u=48323dabcc77b148e26878bc5e0ae015b4618c4f&v=4"
+            alt="Avatar"
+            />
+
+            <div>
+              <strong>Rockeseat_unform</strong>
+              <p>Este repositori foi criado com o intuiro de levar conhecimento aos usuarios. </p>
+            </div>
+
+            <FiChevronRight size={20} />
+
+          </a>
+      </Repositories>
+    </>
   )
 };
 
