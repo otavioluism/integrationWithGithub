2c5e450e486e9bf48a9be7fab7df0d84192a5a25
Otávio Luis Martins <otavioluism97@hotmail.com>
2021-03-09 14:31:11 -0300

    created the routes of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './routes';
 
 function App() {
   return (
-    <h1>hello world</h1>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   );
 }
 
