9985fcefc85dc0791daaff913153830d70a6e37f
Otávio Luis Martins <otavioluism97@hotmail.com>
2021-03-10 11:31:49 -0300

    created connection of a route for ohter

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>
+  const { params } = useRouteMatch<RepositoryParams>(); //pega o params passado pela rota de dashboard e instanciando nas routes
+
+  return <h1>Repository: { params.repository } </h1>
 };
 
 export default Repository;
